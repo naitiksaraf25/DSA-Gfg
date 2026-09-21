02c8789a04bb34fd4834236b196fa9a244b89262
Naitik Saraf <naitiksaraf2507@gmail.com>
2026-09-21 21:59:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Prime Number/next-prime-number.cpp
@@ -0,0 +1,24 @@
+int isPrime(int n)
+{
+	if (n <= 1)return false;
+	if (n <= 3)return true;
+	if (n%2 == 0 || n%3 == 0)return false;
+	
+	for (int i = 5; i*i <= n; i++)
+		{
+		if (n%i == 0)return false;
+	}
+	return true;
+}
+int nextPrime(int n) {
+	int num = n+1;
+	while(true)
+	{
+	    if(isPrime(num))
+	    {
+	        return num;
+	    }
+	    num++;
+	}
+	return -1;
+}
